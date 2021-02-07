85371f36991ae422806d212fce8a7e215d917264
Dmitry <kronalf@inbox.ru>
2021-02-07 21:52:12 +0300

    Add link to product page

--- shop/views.py
+++ shop/views.py
@@ -11,8 +11,11 @@ def home(request, category_slug=None):
 			category=category_page, available=True)
 	else:
 		products = Product.objects.all().filter(available=True)
-	return render(request, 'shop/home.html', {
-		'category':category_page, 'products':products})
+	return render(request, 'shop/home.html', {'category':category_page, 'products':products})
 
-def product(request):
-	return render(request, 'shop/product.html')
+def product(request, category_slug, product_slug):
+	try:
+		product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+	except Exception as e:
+		raise e
+	return render(request, 'shop/product.html', {'product':product})
